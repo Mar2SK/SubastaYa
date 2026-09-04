cb709f515bb8bbe2f26c8d34a1e6becbe67a50f1
Mar2SK <martinpiaggi1212@gmail.com>
2026-09-04 20:27:35 -0300

    feat: configurar EF Core y base de datos inicial

---
+++ SubastaYa.Api/Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SubastaYa.Api.Models;
+
+namespace SubastaYa.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasIndex(user => user.Email).IsUnique();
+
+            entity.Property(user => user.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            entity.Property(user => user.Email)
+                .HasMaxLength(150)
+                .IsRequired();
+        });
+    }
+}
